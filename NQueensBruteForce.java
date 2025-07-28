8218bd5d4b4d9458ecfe9b4344bb221b35aa3fe1
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-28 21:02:53 +0530

    add

--- NQueensBruteForce.java
+++ NQueensBruteForce.java
@@ -1,25 +1,10 @@
-public class NQueensBruteForce {
-    public static void main(String[] args) {
-        int n = 4; // You can change N here
-        char[][] board = new char[n][n];
-
-        // Initialize board with '.'
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = '.';
-            }
-        }
-
-        solveNQueens(board, 0);
-    }
-
+public class NQueensBruteForce {    
     // Recursive function to solve the problem
     public static void solveNQueens(char[][] board, int col) {
         if (col == board.length) {
             printBoard(board);
             return;
         }
-
         for (int row = 0; row < board.length; row++) {
             if (isSafe(board, row, col)) {
                 board[row][col] = 'Q'; // Place queen
@@ -28,7 +13,6 @@ public class NQueensBruteForce {
             }
         }
     }
-
     // Check if placing a queen is safe
     public static boolean isSafe(char[][] board, int row, int col) {
         // Check this row on left side
@@ -37,24 +21,20 @@ public class NQueensBruteForce {
                 return false;
             }
         }
-
         // Check upper-left diagonal
         for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
-
         // Check lower-left diagonal
         for (int i = row, j = col; i < board.length && j >= 0; i++, j--) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
-
         return true;
     }
-
     // Print the board
     public static void printBoard(char[][] board) {
         System.out.println("------ Solution ------");
@@ -64,5 +44,16 @@ public class NQueensBruteForce {
             }
             System.out.println();
         }
+    }
+    public static void main(String[] args) {
+        int n = 4; // You can change N here
+        char[][] board = new char[n][n];
+        // Initialize board with '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        solveNQueens(board, 0);
     }
 }
