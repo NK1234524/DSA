3105ed7b50d54278bdb7d1f71b418609e584c19d
Nishant kumar <nishant201kashyap@gmail.com>
2025-08-21 23:02:57 +0530

    add

---
+++ Size.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+public class Size{
+    public static void main(String args[]){
+        ArrayList<Integer>List = new ArrayList<>();
+        List.add(1);
+        List.add(2);
+        List.add(3);
+        List.add(4);
+        List.add(5);
+        List.add(6);
+        System.out.println(List);
+
+        System.out.println("The size of an array is : "+List.size());
+
+        for(int i=0;i<List.size();i++){
+            System.out.print(List.get(i)+" ");
+        }
+    }
+}
