cc4edee17a883d2ab226de53aa8e3c232d7c7491
Nishant kumar <nishant201kashyap@gmail.com>
2025-08-21 22:46:32 +0530

    add

--- Introduction.java
+++ Introduction.java
@@ -10,14 +10,14 @@ public class Introduction{
         List.add(5);
 
         System.out.println("The Array List is : "+List);
-    //    //Get
-    //    int element = List.get(3);
-    //    System.out.println("The element of an index is : "+element);
+       //Get
+       int element = List.get(3);
+       System.out.println("The element of an index is : "+element);
 
-    //    //Remove
-    //    int element_remove = List.remove(3);
-    //    System.out.println("The removed element is : "+element_remove);
-    //    System.out.println(List);
+       //Remove
+       int element_remove = List.remove(3);
+       System.out.println("The removed element is : "+element_remove);
+       System.out.println(List);
 
        //Set
        List.set(4,10);
