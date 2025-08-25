b95a7788b39940a953466da10d563859e6233a29
Nishant Kumar <nishant201kashyap@gmail.com>
2025-08-25 12:36:44 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -36,6 +36,26 @@ public class LinkedList{
         tail = newNode;
     }
 
+
+
+    public void add(int index, int data){
+        if(index==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i=0;
+        while(i<index-1){
+            temp=temp.next;
+            i++;
+        }
+        newNode.next = temp.next;
+        temp.next=newNode;
+    }
+
+
+    
     public void Print(){
         if(head==null){
             System.out.println("LL is empty here");
@@ -61,4 +81,5 @@ public class LinkedList{
         ll.addLast(4);
         ll.Print();        
     }
+
 }
