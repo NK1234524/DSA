c6e02fedbd31dc0f797dd430b2f2aecfe8c6c2db
Nishant Kumar <nishant201kashyap@gmail.com>
2025-08-25 10:48:32 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,64 @@
+public class LinkedList{
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        //step1 : create new node 
+        Node newNode =new Node(data);
+        //If the Linked List is empty
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }                      
+        //step2 : newNode next = head
+        newNode.next = head;
+        //step 3: head = newNode
+        head = newNode;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail =newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void Print(){
+        if(head==null){
+            System.out.println("LL is empty here");
+            return;
+        }
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList(); 
+         
+        ll.addFirst(1);
+         ll.Print();
+        ll.addFirst(2);
+         ll.Print();
+        ll.addLast(3);
+         ll.Print();
+        ll.addLast(4);
+        ll.Print();        
+    }
+}
