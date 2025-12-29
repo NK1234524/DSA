e66fa12e316a1daf56803b028b1e90c0dc857f0d
Nishant kumar <nishant201kashyap@gmail.com>
2025-12-29 20:20:59 +0530

    add

--- Queues.java
+++ Queues.java
@@ -1,6 +1,24 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Queues {
 
     public static void main(String args []){
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
         
+
+        while(!q.isEmpty()){
+            System.out.print(q.peek()+"<-");
+            q.remove();
+        }
+
     }
 }
