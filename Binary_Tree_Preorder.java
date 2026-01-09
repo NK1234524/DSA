9bf920b0945aa91e78209a858eacccd0d3afc2bc
Nishant kumar <nishant201kashyap@gmail.com>
2026-01-09 20:45:16 +0530

    Add

--- Binary_Tree_Preorder.java
+++ Binary_Tree_Preorder.java
@@ -12,11 +12,30 @@ public class Binary_Tree_Preorder {
     //Preorder Traversing Ro->L->R
     public static void Preorder(Node root){
         if(root == null){
+         //   System.out.print(-1+","+" ");
             return ;
         }
         System.out.print(root.data+" ");
         Preorder(root.left);
         Preorder(root.right);
+    }
+
+      public static void Inorder(Node root){
+        if(root == null){
+            return ;
+        }
+        Inorder(root.left);
+        System.out.print(root.data+" ");
+        Inorder(root.right);
+    }
+
+    public static void PostOrder(Node root){
+        if(root == null){
+            return ;
+        }
+        PostOrder(root.left);
+        PostOrder(root.right);
+        System.out.print(root.data+" ");
     }
     public static void main(String[] args) {
         int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
